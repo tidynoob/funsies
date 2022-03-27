1eba9eba9c65b7f3c65c9660e7e4dc7dfcda74d7
tidynoob <mitchellgriffin9@gmail.com>
2022-03-27 15:58:43 -0400

    more pics and data

--- saint_peters/offensive_rating.R
+++ saint_peters/offensive_rating.R
@@ -7,21 +7,15 @@ ggthemr('fresh')
 
 `%!in%` <- Negate(`%in%`)
 
-sp_image <- "images/saint_peter.png"
-ky_image <- "images/kentucky.png"
-ms_image <- "images/murray_state.png"
-pd_image <- "images/purdue.png"
-
-mm_stats <- readxl::read_excel('alt_stats.xlsx')
+mm_stats <- readxl::read_excel('off_rating.xlsx')
 team_data <- read_csv("team_data.csv")
 
 mm_stats_2 <- mm_stats |> 
   mutate(date = ymd(date)) |> 
-  pivot_longer(3:8,"stat",values_to = "mm_rating") |> 
-  group_by(school,stat) |> 
-  summarise(mm_rating = mean(mm_rating))
+  group_by(school,image) |> 
+  summarise(mm_rating = mean(ortg))
 
-teams <- c("Saint Peter's", "Kentucky", "Purdue", "Murray State")
+teams <- c("Saint Peter's", "Kentucky", "North Carolina", "Kansas", "Miami (FL)", "Houston", "Villanova", "Arkansas", "Duke")
 
 team_data |> 
   # mutate(sp = if_else(school == "Saint Peter's", "Saint Peter's", "Other")) |> 
@@ -29,12 +23,7 @@ team_data |>
   pivot_longer(4:9,"stat",values_to = "rating") |> 
   select(school,stat,rating) |> 
   filter(stat %in% c('ortg')) |> 
-  left_join(mm_stats_2, by = c('school','stat')) |> 
-  mutate(image = case_when(
-    school == "Saint Peter's" ~ sp_image,
-    school == "Kentucky" ~ ky_image,
-    school == "Murray State" ~ ms_image,
-    school == "Purdue" ~ pd_image)) |> 
+  left_join(mm_stats_2, by = c('school')) |> 
   group_by(stat) |> 
   # mutate(std_rating = (rating - mean(rating)) / sd(rating),
   #        mm_std_rating = (mm_rating - mean(rating)) / sd(rating)) |> 
@@ -46,8 +35,8 @@ team_data |>
   ggplot(aes(x = rating, y = mm_rating)) +
   geom_point(size = 2) +
   geom_image(aes(image = image)) +
-  geom_abline(slope = 1,intercept = 0, alpha = 0.8, linetype = 'dashed') +
-  xlim(c(90,120)) +
-  ylim(c(90,120))
+  geom_abline(slope = 1,intercept = 0, alpha = 0.8, linetype = 'dashed')
+  # xlim(c(90,120)) +
+  # ylim(c(90,120))
   # facet_wrap(vars(period)) +
   # scale_alpha_manual(values = c(0.8, .25))
